7d42b03143f06a4d3eceb6c874a0db19cbe70915
Tommy Builds <tommybuilds451@gmail.com>
2024-06-12 09:19:40 -0700

    Get CLI run server command working

--- cli/src/commands/run.rs
+++ cli/src/commands/run.rs
@@ -10,8 +10,12 @@ use crate::util::WasmDevServer;
 use crate::Environment;
 
 pub async fn execute(working_directory: &str, environment: &Environment) -> Result<(), Error> {
+    let cwd_path = Path::new(working_directory);
+    let build_wasm_path = Path::new(&"build-wasm");
+    let build_wasm_wd = cwd_path.join(build_wasm_path);
+
     match environment {
         Environment::Native => panic!("not yet implemented"),
-        Environment::Wasm => WasmDevServer::start().await,
+        Environment::Wasm => WasmDevServer::start(build_wasm_wd.to_str().unwrap()).await,
     }
 }
--- cli/src/util/wasm_dev_server.rs
+++ cli/src/util/wasm_dev_server.rs
@@ -4,20 +4,66 @@ use warp::{http::Response, Filter};
 
 pub struct WasmDevServer;
 
-const INDEX_FILE: &[u8; 658] = include_bytes!("../../../runtime/functor-runtime-web/index.html");
+const INDEX_HTML: &[u8] = include_bytes!("../../../runtime/functor-runtime-web/index.html");
+const JS_FILE_1: &[u8] =
+    include_bytes!("../../../runtime/functor-runtime-web/pkg/functor_runtime_web_bg.js");
+const WASM_FILE: &[u8] =
+    include_bytes!("../../../runtime/functor-runtime-web/pkg/functor_runtime_web_bg.wasm");
+const JS_FILE_2: &[u8] =
+    include_bytes!("../../../runtime/functor-runtime-web/pkg/functor_runtime_web.js");
 
 impl WasmDevServer {
-    pub async fn start() -> Result<(), io::Error> {
+    pub async fn start(working_directory: &str) -> Result<(), io::Error> {
+        let wd = working_directory.to_owned();
         // Match any request and return hello world!
         // let routes = warp::any().map(|| "Hello, World!");
 
-        let index_html = warp::path("index.html").map(move || {
-            Response::builder()
-                .header("Content-Type", "text/html")
-                .body(INDEX_FILE.to_vec());
-        });
+        println!("Starting dev server");
 
-        let routes = index_html;
+        fn create_route(
+            path: &'static str,
+            content: &'static [u8],
+            content_type: &'static str,
+        ) -> warp::filters::BoxedFilter<(impl warp::Reply,)> {
+            warp::path(path)
+                .map(move || {
+                    Response::builder()
+                        .header("Content-Type", content_type)
+                        .body(content.to_vec())
+                })
+                .boxed()
+        }
+
+        // Define routes for each file
+        let route_index = create_route("index.html", INDEX_HTML, "text/html");
+        let route_js1 = warp::path!("pkg" / "functor_runtime_web_bg.js")
+            .map(|| {
+                Response::builder()
+                    .header("Content-Type", "application/javascript")
+                    .body(JS_FILE_1.to_vec())
+            })
+            .boxed();
+        let route_wasm = warp::path!("pkg" / "functor_runtime_web_bg.wasm")
+            .map(|| {
+                Response::builder()
+                    .header("Content-Type", "application/wasm")
+                    .body(WASM_FILE.to_vec())
+            })
+            .boxed();
+        let route_js2 = warp::path!("pkg" / "functor_runtime_web.js")
+            .map(|| {
+                Response::builder()
+                    .header("Content-Type", "application/javascript")
+                    .body(JS_FILE_2.to_vec())
+            })
+            .boxed();
+
+        // Route to serve files from the specified working directory
+        let route_filesystem = warp::fs::dir(wd);
+
+        // Combine all routes
+        let static_routes = route_index.or(route_js1).or(route_wasm).or(route_js2);
+        let routes = static_routes.or(route_filesystem);
 
         warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
         Ok(())
