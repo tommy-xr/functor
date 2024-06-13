b654ee014faf0e4c423f3b7d03302a6a384b0e5c
Tommy Builds <tommybuilds451@gmail.com>
2024-06-13 06:58:44 -0700

    Initial refactoring for hot reload game

--- runtime/functor-runtime-desktop/src/game.rs
+++ runtime/functor-runtime-desktop/src/game.rs
@@ -1,5 +1,7 @@
 use functor_runtime_common::{FrameTime, Scene3D};
 
 pub trait Game {
+    fn check_hot_reload(&mut self, frame_time: FrameTime);
+
     fn render(&mut self, frame_time: FrameTime) -> Scene3D;
 }
--- runtime/functor-runtime-desktop/src/hot_reload_game.rs
+++ runtime/functor-runtime-desktop/src/hot_reload_game.rs
@@ -1,25 +1,43 @@
+use notify::{event, RecursiveMode, Watcher};
 use std::env;
 use std::path::Path;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread::JoinHandle;
 use std::{fs, process};
 use tempfile::tempdir;
 
 use functor_runtime_common::{FrameTime, OpaqueState, Scene3D};
 use libloading::{Library, Symbol};
 
-use crate::game::Game;
+use crate::game::{self, Game};
 
-pub struct HotReloadGame<'a> {
+pub struct HotReloadGame {
     // Utils for constructing the next lib path
     file_stem: String,
     extension: String,
     counter: u32,
 
     // Current hot reload state
-    latest_lib_path: &'a str,
+    latest_lib_path: Arc<String>,
     library: Option<Library>,
+
+    // Hot reload state
+    owned_game_path: Arc<String>,
+    file_changed: Arc<AtomicBool>,
+    watcher_thread: JoinHandle<()>,
 }
 
-impl<'a> Game for HotReloadGame<'a> {
+impl Game for HotReloadGame {
+    fn check_hot_reload(&mut self, frame_time: FrameTime) {
+        if self.file_changed.load(Ordering::SeqCst) {
+            println!("Reloading!");
+            self.file_changed.store(false, Ordering::SeqCst);
+            self.reload();
+            println!("Rendering: {:?}", self.render(frame_time.clone()));
+        }
+    }
+
     fn render(&mut self, frameTime: FrameTime) -> Scene3D {
         // println!("Rendering");
         unsafe {
@@ -29,10 +47,11 @@ impl<'a> Game for HotReloadGame<'a> {
         }
     }
 }
-impl<'a> HotReloadGame<'a> {
-    pub fn create(path: &str) -> HotReloadGame {
+impl HotReloadGame {
+    pub fn create(path: &str, game_path: &str) -> HotReloadGame {
+        let path = Arc::new(path.to_string());
         let counter = 0;
-        let lib_path = Path::new(path);
+        let lib_path = Path::new(path.as_str());
         let file_stem = lib_path
             .file_stem()
             .map(|s| s.to_string_lossy().to_string())
@@ -51,11 +70,59 @@ impl<'a> HotReloadGame<'a> {
 
         let library = unsafe {
             println!("Running initial init.");
-            let lib = Library::new(&path).unwrap();
+            let lib = Library::new(path.as_str()).unwrap();
             let init_func: Symbol<fn()> = lib.get(b"init").unwrap(); // Get the function pointer
             init_func();
             Some(lib)
         };
+
+        let file_changed = Arc::new(AtomicBool::new(false));
+        let file_changed_watcher = Arc::clone(&file_changed);
+        let owned_game_path = Arc::new(game_path.to_string());
+        let owned_game_path2 = owned_game_path.clone();
+
+        let watcher_thread = std::thread::spawn(move || {
+            // Select recommended watcher for debouncer.
+            // Using a callback here, could also be a channel.
+
+            let (tx, rx) = std::sync::mpsc::channel();
+            let mut watcher = notify::recommended_watcher(tx).unwrap();
+
+            let mut had_remove_event = false;
+
+            let path = Path::new(owned_game_path.as_str());
+            watcher.watch(&path, RecursiveMode::Recursive).unwrap();
+
+            println!("watcher created!");
+            loop {
+                match rx.recv() {
+                    Ok(event) => {
+                        match event {
+                            Ok(event) => {
+                                match event.kind {
+                                    event::EventKind::Remove(_) => had_remove_event = true,
+                                    event::EventKind::Create(_) => {
+                                        if had_remove_event {
+                                            had_remove_event = false;
+                                            println!("Pushing hot reload event from thread...");
+                                            file_changed_watcher.store(true, Ordering::SeqCst);
+                                        } else {
+                                            println!("ignoring event");
+                                        }
+                                    }
+                                    _ => (),
+                                };
+                                // TODO: Can we parse events here to handle create -> restore loop?
+                                println!("event: {:?}", event);
+                                //file_changed_watcher.store(true, Ordering::SeqCst);
+                            }
+                            Err(e) => println!("watch error: {:?}", e),
+                        }
+                    }
+                    Err(e) => println!("watch error: {:?}", e),
+                }
+            }
+        });
 
         HotReloadGame {
             file_stem,
@@ -64,6 +131,10 @@ impl<'a> HotReloadGame<'a> {
 
             latest_lib_path: path,
             library,
+
+            owned_game_path: owned_game_path2,
+            watcher_thread,
+            file_changed,
         }
     }
 
@@ -95,8 +166,8 @@ impl<'a> HotReloadGame<'a> {
             println!("Copying to: {}", &temp_dir.path().to_str().unwrap());
             let new_file_name = temp_dir.path().join(self.get_next_destination_name());
             // TODO: Better definition of path here - don't hardcode target/debug
-            let source_path = self.latest_lib_path;
-            fs::copy(source_path, &new_file_name).expect("Cmtpy should succeed");
+            let source_path = &self.latest_lib_path;
+            fs::copy(source_path.as_str(), &new_file_name).expect("Cmtpy should succeed");
             println!("Loading from: {:?}", &new_file_name);
 
             let lib = Library::new(&new_file_name).unwrap();
