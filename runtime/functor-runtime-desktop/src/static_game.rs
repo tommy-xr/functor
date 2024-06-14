bfae2f390b53fc2e40e6aa70df585d4b751f0870
Tommy Builds <tommybuilds451@gmail.com>
2024-06-14 16:01:46 -0700

    Fail compilation with strict

--- runtime/functor-runtime-desktop/src/static_game.rs
+++ runtime/functor-runtime-desktop/src/static_game.rs
@@ -1,32 +1,23 @@
-use notify::{event, RecursiveMode, Watcher};
-use std::env;
-use std::path::Path;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
-use std::thread::JoinHandle;
-use std::{fs, process};
-use tempfile::tempdir;
-
-use functor_runtime_common::{FrameTime, OpaqueState, Scene3D};
+use functor_runtime_common::{FrameTime, Scene3D};
 use libloading::{Library, Symbol};
 
-use crate::game::{self, Game};
+use crate::game::Game;
 
 pub struct StaticGame {
     library: Library,
 }
 
 impl Game for StaticGame {
-    fn check_hot_reload(&mut self, frame_time: FrameTime) {
+    fn check_hot_reload(&mut self, _frame_time: FrameTime) {
         // Noop - nothing to do
     }
 
-    fn render(&mut self, frameTime: FrameTime) -> Scene3D {
+    fn render(&mut self, frame_time: FrameTime) -> Scene3D {
         // println!("Rendering");
         unsafe {
             let render_func: Symbol<fn(FrameTime) -> Scene3D> =
                 self.library.get(b"test_render").unwrap();
-            render_func(frameTime)
+            render_func(frame_time)
         }
     }
 
