51e1780c25723c92ab87c7f4453af95a1533f570
tommy-xr <tommybuilds451@gmail.com>
2024-06-14 16:13:00 -0700

    refactor(runtime): Simplify desktop runtime (#21)

--- runtime/functor-runtime-desktop/src/main.rs
+++ runtime/functor-runtime-desktop/src/main.rs
@@ -77,85 +77,13 @@ pub fn main() {
             window.set_cursor_pos_polling(true);
             window.set_framebuffer_size_polling(true);
             // window.set_cursor_mode(glfw::CursorMode::Disabled);
-            // let gl = glow::Context::from_loader_function_cstr(|s| gl_display.get_proc_address(s));
+
             let gl =
                 glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
-            // gl_surface
-            //     .set_swap_interval(&gl_context, SwapInterval::Wait(NonZeroU32::new(1).unwrap()))
-            //     .unwrap();
 
-            (
-                gl,
-                // gl_surface,
-                // gl_context,
-                "#version 410",
-                window,
-                glfw,
-                events,
-                // event_loop,
-            )
+            (gl, "#version 410", window, glfw, events)
         };
 
-        let vertex_array = gl
-            .create_vertex_array()
-            .expect("Cannot create vertex array");
-        gl.bind_vertex_array(Some(vertex_array));
-
-        let program = gl.create_program().expect("Cannot create program");
-
-        let (vertex_shader_source, fragment_shader_source) = (
-            r#"
-            precision mediump float;
-            uniform mat4 world;
-            const vec2 verts[3] = vec2[3](
-                vec2(0.5f, 1.0f),
-                vec2(0.0f, 0.0f),
-                vec2(1.0f, 0.0f)
-            );
-            out vec2 vert;
-            void main() {
-                vert = verts[gl_VertexID];
-                gl_Position = world * vec4(vert - 0.5, 0.0, 1.0);
-            }"#,
-            r#"precision mediump float;
-            in vec2 vert;
-            out vec4 color;
-            void main() {
-                color = vec4(vert, 0.5, 1.0);
-            }"#,
-        );
-
-        let shader_sources = [
-            (glow::VERTEX_SHADER, vertex_shader_source),
-            (glow::FRAGMENT_SHADER, fragment_shader_source),
-        ];
-
-        let mut shaders = Vec::with_capacity(shader_sources.len());
-
-        for (shader_type, shader_source) in shader_sources.iter() {
-            let shader = gl
-                .create_shader(*shader_type)
-                .expect("Cannot create shader");
-            gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
-            gl.compile_shader(shader);
-            if !gl.get_shader_compile_status(shader) {
-                panic!("{}", gl.get_shader_info_log(shader));
-            }
-            gl.attach_shader(program, shader);
-            shaders.push(shader);
-        }
-
-        gl.link_program(program);
-        if !gl.get_program_link_status(program) {
-            panic!("{}", gl.get_program_info_log(program));
-        }
-
-        for shader in shaders {
-            gl.detach_shader(program, shader);
-            gl.delete_shader(shader);
-        }
-
-        gl.use_program(Some(program));
         gl.clear_color(0.1, 0.2, 0.3, 1.0);
 
         gl.enable(glow::DEPTH_TEST);
