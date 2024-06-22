76df10c20ec855866295b7cbd36ed5f382bc83cf
Tommy Builds <tommybuilds451@gmail.com>
2024-06-22 06:50:22 -0700

    Fix paths for native app

--- cli/src/commands/run.rs
+++ cli/src/commands/run.rs
@@ -17,14 +17,14 @@ pub async fn execute(working_directory: &str, environment: &Environment) -> Resu
 
     let target_dir = Path::new(&"target/debug");
     let library_name = libloading::library_filename("game_native");
-    let game_lib = target_dir.join(Path::new(&library_name));
+    let game_lib = build_native_path.join(target_dir.join(Path::new(&library_name)));
 
     match environment {
         Environment::Native => {
             let commands = vec![ShellCommand {
                 prefix: "[Functor Runner]",
                 cmd: functor_runner_exe.to_str().unwrap(),
-                cwd: build_native_wd.to_str().unwrap(),
+                cwd: working_directory,
                 env: vec![],
                 args: vec!["--game-path", game_lib.to_str().unwrap()],
             }];
