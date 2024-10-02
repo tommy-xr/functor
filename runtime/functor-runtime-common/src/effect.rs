67d6620705d58ec25ddfe41957fe4efb2963eed9
Tommy Builds <tommybuilds451@gmail.com>
2024-10-02 08:10:00 -0700

    Add effect map type

--- runtime/functor-runtime-common/src/effect.rs
+++ runtime/functor-runtime-common/src/effect.rs
@@ -1,3 +1,5 @@
+use fable_library_rust::Native_::Func1;
+
 #[derive(Clone)]
 pub enum Effect<T: Clone + 'static> {
     None,
@@ -11,5 +13,12 @@ impl<T: Clone + 'static> Effect<T> {
 
     pub fn wrapped(data: T) -> Effect<T> {
         Effect::Wrapped(data)
+    }
+
+    pub fn map<U: Clone + 'static>(mapping: Func1<T, U>, source: Effect<T>) -> Effect<U> {
+        match source {
+            Effect::None => Effect::None,
+            Effect::Wrapped(v) => Effect::Wrapped(mapping(v)),
+        }
     }
 }
