fe17465119d217df7d672c40323009e2fa93c0d9
Tommy Builds <tommybuilds451@gmail.com>
2024-10-04 08:39:23 -0700

    Get working on web

--- src/Functor.Game/Runtime.fs
+++ src/Functor.Game/Runtime.fs
@@ -39,6 +39,7 @@ module Runtime
 
         [<OuterAttr("wasm_bindgen")>]
         let tick (frameTimeJs: JsValue): unit =
+            FIXME
             let frameTime = frameTimeJs |> UnsafeJsValue.from_js<Time.FrameTime>;
             if currentRunner.IsSome then 
                 frameTime
